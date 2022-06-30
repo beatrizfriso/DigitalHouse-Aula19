8f4f43d6c69cffeffb787219d3e1817e95ab3edf
Beatriz Friso de Paula <beatriz_f.p@hotmail.com>
2022-06-30 16:39:23 -0300

    Fizemos o projeto e os ajustes necessários

---
+++ projeto/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using projeto.ViewModels;
+
+namespace projeto
+{
+
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            // Inicializa o projeto
+            base.Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Define qual será o Controlador padrão
+            // Nota: O controlador MainViewModel ainda não foi criado nessa etapa.
+            base.DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ projeto/ViewModels/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto.ViewModels
+{
+    public class MainViewModel : Screen
+    {
+
+        private string nomeCompleto;
+
+        public string NomeCompleto
+        {
+            get
+            {
+                return nomeCompleto;
+            }
+            set
+            {
+                nomeCompleto = value;
+                // Observa se o atributo/instância FirstName recebeu alguma atualização.
+                base.NotifyOfPropertyChange(() => NomeCompleto);
+            }
+        }
+    }
+}
